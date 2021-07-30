fd2e4180e014736e24a9412e4557c3f0bb9841a0
marcoref3at <marco_refaat@hotmail.com>
2021-07-30 11:58:57 +0200

    Refactoring Details Menu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,44 +39,29 @@
           </template>
         </v-calendar>
 
+        <!-- Event Details Menu -->
         <v-menu
           v-model="selectedOpen"
           :close-on-content-click="false"
           :activator="selectedElement"
           offset-x
         >
-          <v-card color="grey lighten-4" min-width="350px" flat>
-            <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-card-text>
-              <span v-html="selectedEvent.details"></span>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn text color="secondary" @click="selectedOpen = false">
-                Cancel
-              </v-btn>
-            </v-card-actions>
-          </v-card>
+          <EventDetails
+            :selectedOpen="selectedOpen"
+            :selectedElement="selectedElement"
+            :selectedEvent="selectedEvent"
+            @setEventDetailsOpen="setEventDetailsOpen"
+          />
         </v-menu>
       </v-sheet>
     </v-col>
   </v-row>
 </template>
 <script>
+import EventDetails from "./Calendar/EventDetails.vue";
 import CalendarHeader from "./Calendar/CalendarHeader.vue";
 export default {
-  components: { CalendarHeader },
+  components: { CalendarHeader, EventDetails },
   data: () => ({
     focus: "",
     type: "week",
@@ -115,6 +100,9 @@ export default {
     this.$refs.calendar.checkChange();
   },
   methods: {
+    setEventDetailsOpen(value) {
+      this.selectedOpen = value;
+    },
     setCalendarType(newType) {
       this.type = newType;
     },
