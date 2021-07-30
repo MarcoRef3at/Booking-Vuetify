4d3666afe1c5168b85b8186bc1a73ea775f90e0f
marcoref3at <marco_refaat@hotmail.com>
2021-07-30 11:48:17 +0200

    Refactore Header

---
+++ src/components/Calendar/CalendarHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <v-sheet height="64">
+      <v-toolbar flat>
+        <!-- Today Button -->
+        <v-btn
+          outlined
+          class="mr-4"
+          color="grey darken-2"
+          @click="$emit('setToday')"
+        >
+          Today
+        </v-btn>
+
+        <!-- Previous Button -->
+        <v-btn fab text small color="grey darken-2" @click="$emit('prev')">
+          <v-icon small>
+            mdi-chevron-left
+          </v-icon>
+        </v-btn>
+
+        <!-- Next Button -->
+        <v-btn fab text small color="grey darken-2" @click="$emit('next')">
+          <v-icon small>
+            mdi-chevron-right
+          </v-icon>
+        </v-btn>
+
+        <!-- Calendar Title -->
+        <v-toolbar-title v-if="calendar">
+          {{ calendar.title }}
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <!-- Calendar Type Menu -->
+        <v-menu bottom right>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+              <span>{{ typeToLabel[type] }}</span>
+              <v-icon right>
+                mdi-menu-down
+              </v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="$emit('setCalendarType', 'day')">
+              <v-list-item-title>Day</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$emit('setCalendarType', 'week')">
+              <v-list-item-title>Week</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$emit('setCalendarType', 'month')">
+              <v-list-item-title>Month</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$emit('setCalendarType', '4day')">
+              <v-list-item-title>4 days</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["calendar", "type"],
+  data() {
+    return {
+      typeToLabel: {
+        month: "Month",
+        week: "Week",
+        day: "Day",
+        "4day": "4 Days"
+      }
+    };
+  }
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,51 +1,14 @@
 <template>
   <v-row class="fill-height">
     <v-col>
-      <v-sheet height="64">
-        <v-toolbar flat>
-          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
-            Today
-          </v-btn>
-          <v-btn fab text small color="grey darken-2" @click="prev">
-            <v-icon small>
-              mdi-chevron-left
-            </v-icon>
-          </v-btn>
-          <v-btn fab text small color="grey darken-2" @click="next">
-            <v-icon small>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu bottom right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>
-                  mdi-menu-down
-                </v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
+      <CalendarHeader
+        @setToday="setToday"
+        @prev="prev"
+        @next="next"
+        :calendar="$refs.calendar"
+        :type="type"
+        @setCalendarType="setCalendarType"
+      />
 
       <v-sheet height="600">
         <v-calendar
@@ -111,16 +74,13 @@
   </v-row>
 </template>
 <script>
+import CalendarHeader from "./Calendar/CalendarHeader.vue";
 export default {
+  components: { CalendarHeader },
   data: () => ({
     focus: "",
-    type: "month",
-    typeToLabel: {
-      month: "Month",
-      week: "Week",
-      day: "Day",
-      "4day": "4 Days"
-    },
+    type: "week",
+
     selectedEvent: {},
     selectedElement: null,
     selectedOpen: false,
@@ -155,6 +115,9 @@ export default {
     this.$refs.calendar.checkChange();
   },
   methods: {
+    setCalendarType(newType) {
+      this.type = newType;
+    },
     startDrag({ event, timed }) {
       if (event && timed) {
         this.dragEvent = event;
