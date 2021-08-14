14fb3d829d968bb9e2d248bef7abd03a8a006a8e
marcoref3at <marco_refaat@hotmail.com>
2021-08-14 14:08:07 +0200

    Bootstrap

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import vuetify from "./plugins/vuetify";
 import store from "./store";
 import router from "./router";
 import LottieAnimation from "lottie-vuejs/src/LottieAnimation.vue"; // import lottie-vuejs
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
 
