24795e3d606badffc3453222900752c2d5a06fb7
marcoref3at <marco_refaat@hotmail.com>
2021-08-16 10:50:27 +0200

    refactoring

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,9 +1,57 @@
-export const dateRangeOverlaps = (a_start, a_end, b_start, b_end) => {
-  if (a_start < b_start && b_start < a_end) return true; // b starts in a
-  if (a_start < b_end && b_end < a_end) return true; // b ends in a
-  if (b_start < a_start && a_end < b_end) return true; // a in b
-  if (a_start == b_start && a_end >= b_end) return true; //a = b
-  if (b_start < a_start && a_end == b_end) return true;
-  if (a_start == b_start && b_end >= a_end) return true;
+let intersections = [];
+export const getDateRangesIntersection = timeEntries => {
+  let i = 0,
+    j = 0;
+  let timeIntervals = timeEntries.filter(
+    entry => entry.start != null && entry.end != null
+  );
+
+  if (timeIntervals != null && timeIntervals.length > 1)
+    for (i = 0; i < timeIntervals.length - 1; i += 1) {
+      for (j = i + 1; j < timeIntervals.length; j += 1) {
+        if (
+          dateRangeOverlaps(
+            new Date(timeIntervals[i].start).getTime(),
+            new Date(timeIntervals[i].end).getTime(),
+            new Date(timeIntervals[j].start).getTime(),
+            new Date(timeIntervals[j].end).getTime()
+          )
+        )
+          return intersections;
+      }
+    }
+  return intersections;
+};
+
+const dateRangeOverlaps = (a_start, a_end, b_start, b_end) => {
+  if (
+    (a_start < b_start && b_start < a_end) ||
+    (a_start < b_end && b_end < a_end) ||
+    (b_start < a_start && a_end < b_end) ||
+    (a_start == b_start && a_end >= b_end) ||
+    (b_start < a_start && a_end == b_end) ||
+    (a_start == b_start && b_end >= a_end)
+  ) {
+    return intersections.push(
+      getIntersection(
+        { start: a_start, end: a_end },
+        { start: b_start, end: b_end }
+      )
+    );
+  }
+
   return false;
+};
+
+const getIntersection = (a, b) => {
+  let min = a.start < b.start ? a : b;
+  let max = min == a ? b : a;
+
+  //min ends before max starts -> no intersection
+  if (min.end < max.start) return null; //the ranges don't intersect
+
+  return {
+    start: max.start,
+    end: min.end < max.end ? min.end : max.end
+  };
 };
--- src/store/events-store.js
+++ src/store/events-store.js
@@ -2,7 +2,7 @@ import { corsBridge, paymobApi } from "../api/client";
 import config from "../api/config";
 import endpoints from "../api/endpoints";
 import getPaymobIFrameToken from "../api/paymob_request";
-import { dateRangeOverlaps } from "./../functions/index";
+import { getDateRangesIntersection } from "./../functions/index";
 const state = {
   eventsArr: [],
   selectedEvent: {},
@@ -100,63 +100,7 @@ const actions = {
       if (court == null) {
         // Let duplicated values only in blocked array
 
-        const multipleDateRangeOverlaps = timeEntries => {
-          let duplicates = [];
-          const getIntersection = (a, b) => {
-            let min = a.start < b.start ? a : b;
-            let max = min == a ? b : a;
-
-            //min ends before max starts -> no intersection
-            if (min.end < max.start) return null; //the ranges don't intersect
-
-            return {
-              start: max.start,
-              end: min.end < max.end ? min.end : max.end
-            };
-          };
-          // TODO get smaller date range
-          const dateRangeOverlaps = (a_start, a_end, b_start, b_end) => {
-            if (
-              (a_start < b_start && b_start < a_end) ||
-              (a_start < b_end && b_end < a_end) ||
-              (b_start < a_start && a_end < b_end) ||
-              (a_start == b_start && a_end >= b_end) ||
-              (b_start < a_start && a_end == b_end) ||
-              (a_start == b_start && b_end >= a_end)
-            ) {
-              return duplicates.push(
-                getIntersection(
-                  { start: a_start, end: a_end },
-                  { start: b_start, end: b_end }
-                )
-              );
-            }
-
-            return false;
-          };
-          let i = 0,
-            j = 0;
-          let timeIntervals = timeEntries.filter(
-            entry => entry.start != null && entry.end != null
-          );
-
-          if (timeIntervals != null && timeIntervals.length > 1)
-            for (i = 0; i < timeIntervals.length - 1; i += 1) {
-              for (j = i + 1; j < timeIntervals.length; j += 1) {
-                if (
-                  dateRangeOverlaps(
-                    new Date(timeIntervals[i].start).getTime(),
-                    new Date(timeIntervals[i].end).getTime(),
-                    new Date(timeIntervals[j].start).getTime(),
-                    new Date(timeIntervals[j].end).getTime()
-                  )
-                )
-                  return duplicates;
-              }
-            }
-          return duplicates;
-        };
-        blocked = multipleDateRangeOverlaps(blocked);
+        blocked = getDateRangesIntersection(blocked);
       }
 
       blocked.forEach(event => {
