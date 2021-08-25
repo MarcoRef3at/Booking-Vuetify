fdfe2471f0ddc9a7ed9f691d713f7c7b9899e8e7
marcoref3at <marco_refaat@hotmail.com>
2021-08-25 17:14:01 +0200

    changing buil path modified

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 module.exports = {
   outputDir: path.resolve(__dirname, "../Booking-Server/dist"),
-  assetsDir: "../../static",
   transpileDependencies: ["vuetify"]
 };
