42509350d3c35d70a74b20c022a716f3023eeed3
marcoref3at <marco_refaat@hotmail.com>
2021-09-26 14:08:29 +0200

    production

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
 module.exports = {
-  outputDir: path.resolve(__dirname, "../Booking-Server/dist/v2"),
+  outputDir: path.resolve(__dirname, "../Booking-Engine/dist/v2"),
   transpileDependencies: ["vuetify"]
 };
